81e880180057aa14eca09d2cf52b7984f2624117
IsiLuanna <ilnp@discente.ifpe.edu.br>
2026-03-12 10:24:16 -0300

    Update MainActivity.kt

--- atividade1/MainActivity.kt
+++ atividade1/MainActivity.kt
@@ -3,36 +3,101 @@ package com.example.myprojectone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard("Android")
+
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                MessageCard(
+                    Message("Android", "Jetpack Compose", R.drawable.pfp1)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Samuel", "Aprendendo Jetpack Compose", R.drawable.pfp1)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                MessageCard(
+                    Message("Lexi", "Compose é incrível!", R.drawable.pfp1)
+                )
+            }
         }
     }
 }
 
+data class Message(
+    val author: String,
+    val body: String,
+    val image: Int
+)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+
+        Image(
+            painter = painterResource(msg.image),
+            contentDescription = "Foto de perfil",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+
+            Text(text = msg.author)
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Text(text = msg.body)
+        }
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun PreviewMessageCard() {
-    MessageCard("Android")
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        MessageCard(
+            Message("Luanna", "Hey, take a look at Jetpack Compose!", R.drawable.pfp1)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Isi", "Jetpack Compose é muito bom!", R.drawable.pfp1)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MessageCard(
+            Message("Android", "Bem-vindo ao Compose!", R.drawable.pfp1)
+        )
+    }
 }
-
-
-
-
